e44bf12628aa6682c199e3b411c8ad92bb04954e
Guceanu Daniel <guceanudaniel18@gmail.com>
2021-10-26 21:53:57 +0300

    Update real-time capabilities

--- SpeechToTextV2/Form1.Designer.cs
+++ SpeechToTextV2/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace SpeechToTextV2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.textBox = new System.Windows.Forms.RichTextBox();
@@ -36,29 +37,37 @@ namespace SpeechToTextV2
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(229, 267);
+            this.btnStart.BackColor = System.Drawing.Color.Lime;
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(25, 273);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.Size = new System.Drawing.Size(92, 54);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(372, 267);
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.Location = new System.Drawing.Point(319, 273);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.Size = new System.Drawing.Size(94, 54);
             this.btnStop.TabIndex = 1;
             this.btnStop.Text = "Stop";
-            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.UseVisualStyleBackColor = false;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // textBox
             // 
-            this.textBox.Location = new System.Drawing.Point(229, 98);
+            this.textBox.BackColor = System.Drawing.Color.Black;
+            this.textBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox.Location = new System.Drawing.Point(25, 58);
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(218, 135);
+            this.textBox.Size = new System.Drawing.Size(388, 189);
             this.textBox.TabIndex = 2;
             this.textBox.Text = "";
             // 
@@ -66,7 +75,9 @@ namespace SpeechToTextV2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 443);
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(433, 372);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
